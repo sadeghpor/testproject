cacccb78ef60c6d3a6a62525f70ee4d23a49d847
mirsadegh <64655834+mirsadegh@users.noreply.github.com>
2021-09-25 11:04:45 +0330

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -7,11 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['comment' , 'approved' , 'parent_id' ,'commentable_id' , 'commentable_type'];
-
-   
-
-
-
-    
-  
 }
