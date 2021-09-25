1b25baaea186dbc269f2c32141b4c2ad7266b99d
sadegh <mirsadeghporbehnam831@gmail.com>
2021-09-25 10:57:12 +0330

    add app folder

---
+++ app/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['comment' , 'approved' , 'parent_id' ,'commentable_id' , 'commentable_type'];
+
+   
+
+
+
+    
+  
+}
